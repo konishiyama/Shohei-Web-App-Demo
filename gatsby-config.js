f8d36519a2001f9ef231a6d65364332688bc734b
Ko Nishiyama <nishiyama.ko.0420@gmail.com>
2020-06-12 18:19:44 +0900

    200612_2_titleChanged

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Shohei Yamaguchi Official Website',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      '栃木県日光市選出、衆議院議員山口翔平の、公式ウェブサイトです。',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
